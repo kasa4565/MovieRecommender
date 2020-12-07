89eb84b08ac77b73c987374f135f65812d83de92
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2020-12-07 20:20:07 +0100

    Add project files.

---
+++ MovieRecommender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieRecommender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
